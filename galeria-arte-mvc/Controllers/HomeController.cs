705b217531bb66f67de50100edd14cf32ec84848
Gabriel Martinez <gaabi26092005@gmail.com>
2026-02-09 17:11:24 -0300

    Crear vista con cards en el Index del HomeController.

--- galeria-arte-mvc/Controllers/HomeController.cs
+++ galeria-arte-mvc/Controllers/HomeController.cs
@@ -1,21 +1,29 @@
 using System.Diagnostics;
+using galeria_arte_mvc.Data;
 using galeria_arte_mvc.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace galeria_arte_mvc.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly GaleriaDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, GaleriaDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var obras = _context.Obras
+                .Take(6)
+                .Include(o => o.Artista)
+                .ToList();
+            return View(obras);
         }
 
         public IActionResult Privacy()
